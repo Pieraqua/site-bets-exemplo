7fb7ec3127c8a2882a29bce60d204f075e95177a
pieraqua <pauloffxii@hotmail.com>
2024-08-14 12:22:40 -0300

    inicio da tela de bets

---
+++ src/components/CartaoPartida.vue
@@ -0,0 +1,30 @@
+<template>
+<v-card class="pa-2">
+    <v-card-subtitle>{{ data }} | {{ hora }}</v-card-subtitle>
+    <v-card-title>{{ time_1 }}</v-card-title>
+    <v-card-title>{{ time_2 }}</v-card-title>
+    <v-card-actions class="justify-space-between">
+        <v-card width="100%" class="ma-2 pa-1">
+            <div style="float:left">1</div><div style="float:right">{{ odds1 }}</div>
+        </v-card>
+        <v-card width="100%" class="ma-2 pa-1">
+            <div style="float:left">X</div><div style="float:right">{{ oddsEmpate }}</div>
+        </v-card>
+        <v-card width="100%" class="ma-2 pa-1">
+            <div style="float:left">2</div><div style="float:right">{{ odds2 }}</div>
+        </v-card>
+    </v-card-actions>
+</v-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    data : String,
+    hora : String,
+    time_1: String,
+    time_2: String,
+    odds1: Number,
+    odds2: Number,
+    oddsEmpate: Number
+})
+</script>
--- src/pages/bets.vue
+++ src/pages/bets.vue
@@ -1,7 +1,58 @@
 <template>
 
+<v-navigation-drawer rail expand-on-hover>
+    <v-list>
+        <v-list-item v-for="item in items_navegacao" :prepend-icon="item.icone" :title="item.nome"></v-list-item>
+    </v-list>
+</v-navigation-drawer>
+<v-container>
+    <v-row>
+        <v-col>
+            <CartaoPartida class="" time_1="Santos" time_2="Curitiba" :odds1="2.3" :odds2="1.5" :odds-empate="2" data="Hoje" hora="12:00"></CartaoPartida>
+        </v-col>
+        <v-col>
+            <CartaoPartida class="" time_1="Atlético" time_2="Atlético MG" :odds1="2" :odds2="1.5" :odds-empate="2" data="Hoje" hora="12:00"></CartaoPartida>
+        </v-col>
+        <v-col>
+            <CartaoPartida class="" time_1="Laranja Mecânica" time_2="Operário" :odds1="1" :odds2="1" :odds-empate="1.5" data="Hoje" hora="12:00"></CartaoPartida>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
 
 <script lang="ts" setup>
-
+const items_navegacao = [
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+    {
+        nome: 'Bilhetes',
+        icone: 'mdi-ticket'
+    },
+    {
+        nome: 'Apostas por Odds',
+        icone: 'mdi-medal'
+    },
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+    {
+        nome: 'Futebol',
+        icone: 'mdi-soccer'
+    },
+]
 </script>
