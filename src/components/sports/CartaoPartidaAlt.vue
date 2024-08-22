4294ec8d086f2f29affdf347d16cea68b59d93aa
pieraqua <pauloffxii@hotmail.com>
2024-08-21 21:44:20 -0300

    css dos cartões alternativos

--- src/components/sports/CartaoPartidaAlt.vue
+++ src/components/sports/CartaoPartidaAlt.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-card flat class="bg-grey-lighten-2 pa-2 pl-4 pr-2" min-width="300px" rounded="lg" style="font-size:small">
+    <v-card flat class="pa-2 pl-4 pr-2" min-width="300px" rounded="lg" style="font-size:small; background: #e3e8eb">
         <v-container class="pa-0 ma-0">
             <!-- Info campeonato -->
             <v-row no-gutters class="pb-1">
                 <v-col style="opacity: 0.6" cols="12">
-                    <v-btn variant="plain" :ripple="false" class="ma-0 pa-0" size="small">
+                    <v-btn variant="plain" :ripple="false" class="ma-0 pa-0 text-capitalize" size="small">
                         <template v-slot:prepend>
                             <v-icon size="small">mdi-soccer</v-icon>
                         </template>
@@ -19,48 +19,51 @@
             <v-row no-gutters  class="pb-1">
                 <v-col cols="12">
                     <div v-if="partida.statusPartida">
-                        <span style="color:blue; margin-right: 5px">{{ partida.tempo_partida }}</span>
+                        <span style="color:rgb(11, 162, 247); margin-right: 5px" class="font-weight-bold">{{ partida.tempo_partida }}</span>
                         <v-icon color="red" class="float-right pr-3">mdi-access-point</v-icon>
                     </div>
-                    <div v-else style="opacity: 0.6">
+                    <div v-else class="texto-fundo">
                         {{ partida.tempo_partida }}
                     </div>
                 </v-col>
             </v-row>
             <!-- Emblema e nome time 1 -->
-            <v-row no-gutters>
+            <v-row no-gutters align="center">
                 <v-col>
-                    <div class="float-left" style="width:32px; height:32px">
+                    <div style="width:32px; height:32px; display: inline-block">
                         <v-img :src="partida.time_1_icon" max-width="32px" max-height="32px"/>
                     </div>
-                    <span class="float-left mt-2 mb-2 ml-2">
+                    <div class="ml-2 nome-time">
                     {{ partida.time_1 }}
-                    </span>
+                    </div>
 
-                    <v-card v-if="partida.statusPartida" class="float-right pa-1 ma-1 pl-2 pr-2">
-                        {{partida.time_1_pontos}}
-                    </v-card>
                 </v-col>
-
+                <v-col cols="auto">
+                    <div v-if="partida.statusPartida" class="rounded-lg pontos-time text-center">
+                        {{partida.time_1_pontos}}
+                    </div>
+                </v-col>
             </v-row>
             <!-- Emblema e nome time 2 -->
-            <v-row no-gutters  class="pb-1">
+            <v-row no-gutters align="center" justify="space-between" class="pb-1">
                 <v-col >
-                    <div class="float-left" style="width:32px; height:32px">
+                    <div style="width:32px; height:32px; display: inline-block">
                         <v-img :src="partida.time_2_icon" max-width="32px" max-height="32px"/>
                     </div>
-                    <span class="float-left mt-2 mb-2 ml-2">
+                    <span class="ml-2 nome-time">
                     {{ partida.time_2 }}
                     </span>
-                    <v-card v-if="partida.statusPartida" class="float-right pa-1 ma-1 pl-2 pr-2">
+                </v-col>
+                <v-col cols="auto">
+                    <div v-if="partida.statusPartida" class="rounded-lg pontos-time text-center">
                         {{partida.time_2_pontos}}
-                    </v-card>
+                    </div>
                 </v-col>
 
             </v-row>
             <!-- 1x2 -->
             <v-row no-gutters>
-                <v-col style="opacity: 0.6">
+                <v-col style="font-weight: 600; color: #4d4d4d; opacity: 0.5">
                     1x2
                 </v-col>
             </v-row>
@@ -69,7 +72,7 @@
                 <v-col>
                     <v-card :ripple="false" link class="bg-grey-lighten-4 pa-2 mr-1" elevation="0">
                         <v-row>
-                            <v-col cols="auto" class="me-auto">
+                            <v-col cols="auto" class="me-auto texto-fundo">
                                 1
                             </v-col>
                             <v-col cols="auto">
@@ -81,7 +84,7 @@
                 <v-col cols="4">
                     <v-card link :ripple="false" class="bg-grey-lighten-4 pa-2" elevation="0">
                         <v-row>
-                            <v-col cols="auto" class="me-auto">
+                            <v-col cols="auto" class="me-auto texto-fundo">
                                 empate
                             </v-col>
                             <v-col cols="auto">
@@ -93,7 +96,7 @@
                 <v-col>
                     <v-card link :ripple="false" class="bg-grey-lighten-4 pa-2 ml-1" elevation="0">
                         <v-row>
-                            <v-col class="me-auto">
+                            <v-col class="me-auto texto-fundo">
                                 2
                             </v-col>
                             <v-col cols="auto">
@@ -123,4 +126,36 @@ const props = defineProps({
 </script>
 
 <style lang="css">
+.nome-time{
+    overflow: hidden;
+    color: #4d4d4d;
+    font-weight: 600;
+    height:24px;
+    line-height: 24px;;
+    display: inline-block;
+}
+
+.pontos-time{
+    background: rgba(77, 77, 77, 0.05);
+    font-size: 14px;
+    border: 1px solid rgba(77, 77, 77, 0.1);
+    height: 24px;
+    line-height: 24px;
+    align-items: center;
+    font-weight: 600;
+    min-width: 32px;
+    justify-content: center;
+    
+}
+
+.col-mask{
+    mask-image: linear-gradient(90deg, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%);
+    overflow: hidden;
+}
+
+.texto-fundo{
+    font-weight: 600; 
+    opacity: 0.5; 
+    color: #4d4d4d;
+}
 </style>
